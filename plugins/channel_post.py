6f25c65f2df42d5c9a81ad3b79f6bfa970c86b77
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-12-26 17:43:52 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -53,45 +53,9 @@ async def new_post(client: Client, message: Message):
     link = f"https://t.me/{client.username}?start={base64_string}"
     shorty = tiny(shorten_url(link))
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Send in DM", url=f'{shorty}')]])
-
-    media = message.audio or message.video or message.document or message.photo
-    caption = message.caption if message.caption else ""
-    if message.video:
-        new_caption = f"<blockquote><b>{caption}</b></blockquote>\n<blockquote><b>{humanbytes(media.file_size)}</b></blockquote>\n<blockquote><b>{TimeFormatter(media.duration * 1000)}</b></blockquote>"
-    else:
-        new_caption = f"<blockquote><b>{caption}</b></blockquote>\n<blockquote><b>{humanbytes(media.file_size)}</b></blockquote>"
-
     
     try:
-        await asyncio.sleep(15)
-               
-        await client.send_message(NEW_CAPTIONS_CHANNEL_ID, text = new_caption , reply_markup=reply_markup)
-
+        await message.edit_reply_markup(reply_markup)             
     except Exception as e: 
         print(e)
         pass
-
-def humanbytes(size):
-    # Function to format file size in a human-readable format
-    if not size:
-        return "0 B"
-    # Define byte sizes
-    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-    i = 0
-    while size >= 1024 and i < len(suffixes) - 1:
-        size /= 1024
-        i += 1
-    f = ('%.2f' % size).rstrip('0').rstrip('.')
-    return f"{f} {suffixes[i]}"
-
-def TimeFormatter(milliseconds: int) -> str:
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = ((str(days) + " days, ") if days else "") + \
-        ((str(hours) + " hrs, ") if hours else "") + \
-        ((str(minutes) + " min, ") if minutes else "") + \
-        ((str(seconds) + " sec, ") if seconds else "") + \
-        ((str(milliseconds) + " millisec, ") if milliseconds else "")
-    return tmp[:-2]
